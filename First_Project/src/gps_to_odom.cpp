0383902723edf3f8e8391ada8c01936356e5823f
MirkoCalvi <mirkocalvi.job@gmail.com>
2024-04-23 22:58:14 +0200

    new version of the first node, still not working as wanted, I'll keep cr..trying

--- First_Project/src/gps_to_odom.cpp
+++ First_Project/src/gps_to_odom.cpp
@@ -9,170 +9,197 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
-class gps_to_odom
+std::vector<double> vectorial_mult(const std::vector<std::vector<double>>& matrix, const std::vector<double>& vect)
+{
+    std::vector<double> res(matrix.size(), 0);
+
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix.size(); j++)
+        {
+            res[i] += matrix[i][j] * vect[j];
+        }
+    }
+    return res;
+}
+
+class gps_coord
+{
+
+public:
+    double latitude;  // in radiants
+    double longitude; // in radiants
+    double altitude;  // in radiants
+
+    gps_coord(){}
+    gps_coord(double lat, double lng, double alt) // input coords must be in degrees
+    {
+        this->latitude = deg2rad(lat);
+        this->longitude = deg2rad(lng);
+        this->altitude = alt;
+    }
+
+private:
+    double deg2rad(double grad)
+    {
+        double rad = grad * 3.14159 / 180;
+        return rad;
+    }
+
+};
+
+class ECEF_coord
+{
+
+public:
+    double x;
+    double y;
+    double z;
+
+    ECEF_coord(){}
+    ECEF_coord(const gps_coord &gps)
+    {
+        init_const(gps.latitude);
+
+        x = (N + gps.altitude) * cos(gps.latitude) * cos(gps.longitude);
+        y = (N + gps.altitude) * cos(gps.latitude) * sin(gps.longitude);
+        z = (N * (1 - e2) + gps.altitude) * sin(gps.latitude);
+    }
+    ECEF_coord(double x, double y, double z)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+
+    /*ref_syst& operator=(ref_syst&& other) noexcept {
+        if (this != &other) {
+            // Move data members
+            ecef = std::move(other.ecef);
+            gps = std::move(other.gps);
+            matrix = std::move(other.matrix);
+        }
+        return *this;
+    }*/
+
+private:
+    double a = 6378137;
+    double b = 6356752;
+    double N;
+    double e2;
+
+    void init_const(double latitude_in_rad)
+    { // initialize constants
+        e2 = get_e2();
+        N = get_N(latitude_in_rad);
+    }
+
+    double get_N(double latitude)
+    {
+
+        double N;
+        N = a / sqrt(1 - e2 * sin(latitude) * sin(latitude));
+
+        return N;
+    }
+
+    double get_e2()
+    {
+        double e2;
+        e2 = 1 - (b * b) / (a * a);
+        return e2;
+    }
+};
+
+class Ref_syst
+{
+public:
+    ECEF_coord ecef;
+    gps_coord gps;
+    std::vector<std::vector<double>> matrix;
+
+    Ref_syst(){}
+    Ref_syst(const gps_coord& gps_ref_syst, const ECEF_coord& ecef_ref_syst): gps(gps_ref_syst), ecef(ecef_ref_syst), matrix(get_matrix(gps_ref_syst)){}
+
+    // Move assignment operator
+    /*ref_syst& operator=(ref_syst&& other) noexcept {
+        if (this != &other) {
+            // Move data members
+            ecef = std::move(other.ecef);
+            gps = std::move(other.gps);
+            matrix = std::move(other.matrix);
+        }
+        return *this;
+    }*/
+
+private:
+    std::vector<std::vector<double>> get_matrix(const gps_coord& gps)
+    {
+        std::vector<std::vector<double>> matrix{3, std::vector<double>(3)};
+
+        double lambda;
+        double theta;
+
+        lambda = gps.longitude;
+        theta = gps.latitude;
+
+        matrix[0][0] = -sin(lambda);
+        matrix[0][1] = cos(lambda);
+        matrix[0][2] = 0;
+
+        matrix[1][0] = -sin(theta) * cos(lambda);
+        matrix[1][1] = -sin(theta) * sin(lambda);
+        matrix[1][2] = cos(theta);
+
+        matrix[2][0] = cos(theta) * cos(lambda);
+        matrix[2][1] = cos(theta) * sin(lambda);
+        matrix[2][2] = sin(theta);
+
+        return matrix;
+    }
+};
+
+class ENU_coord
+{
+public:
+    double x;
+    double y;
+    double z;
+
+    ENU_coord(){}
+    ENU_coord(const ECEF_coord &ecef, const Ref_syst &ref_syst)
+    { // input coords must be in degrees
+
+        std::vector<double> diff(3);
+        std::vector<double> res(3);
+        diff[0] = ecef.x - ref_syst.ecef.x;
+        diff[1] = ecef.y - ref_syst.ecef.y;
+        diff[2] = ecef.z - ref_syst.ecef.z;
+
+        res = vectorial_mult(ref_syst.matrix, diff);
+
+        x = res[0];
+        y = res[1];
+        z = res[2];
+    }
+    ENU_coord(double x, double y, double z)
+    { 
+        this->x=x;
+        this->y=y;
+        this->z=z;
+    }
+};
+
+class gps_to_odom // class of the node
 {
 
 private:
-    ros::NodeHandle n;
-    ros::Subscriber sub;
-    ros::Publisher pub;
-    bool flag;
 
-    // reference system data in radiants
-    double ref_latitude;
-    double ref_longitude;
-    double ref_altitude;
-
-    std::vector<double> ref_ecef{3, 0.0};                                   // ecef format of reference system
-    std::vector<double> ref_enu{3, 0.0};                                    // enu format of reference system
-    std::vector<std::vector<double>> ref_matrix{3, std::vector<double>(3)}; // Matrix to obtain ENU
-
-    std::vector<double> prec_pose{3, 0}; // ENU format, precedent position of the robot, necessary to calculate orientation
-    int odom_seq_id;
-
-public:
-    gps_to_odom()
+    geometry_msgs::Quaternion get_orientation(ENU_coord& curr_pose, ENU_coord& prec_pose)
     {
-        flag = false;
-        sub = n.subscribe("/fix", 1, &gps_to_odom::callback, this);
-        pub = n.advertise<nav_msgs::Odometry>("/gps_odom", 1);
-
-        ros::Rate loop_rate(5);
-
-        while (ros::ok())
-        {
-            ros::spinOnce();
-            loop_rate.sleep();
-        }
-    }
-
-    void callback(const sensor_msgs::NavSatFix::ConstPtr &msg)
-    {
-        nav_msgs::Odometry odom;
-
-        double latitude = msg->latitude * 3.14159 / 180;
-        double longitude = msg->longitude * 3.14159 / 180;
-        double altitude = msg->altitude;
-
-        std::vector<double> ecef;
-        std::vector<double> enu; //(x, y, z, orientation)
-        tf2::Quaternion quat_orientation;
-
-        if (!flag)
-        {
-            this->set_reference_system(latitude, longitude, altitude);
-            prec_pose = ref_enu;
-            flag = true;
-            odom_seq_id = 0;
-            ROS_INFO("----------reference system\n lat=%f , long=%f, height=%f\n enu: x=%f , y=%f , z=%f\n", latitude, longitude, altitude, odom.pose.pose.position.x, odom.pose.pose.position.y, odom.pose.pose.position.z);
-        }
-
-        ecef = this->from_gps_to_ECEF(latitude, longitude, altitude);
-        enu = this->from_ECEF_to_ENU(ecef);
-
-        // creating the odometry
-        // header
-        odom.header.seq = odom_seq_id;
-        odom.header.stamp = ros::Time ::now();
-        odom.header.frame_id = "gps_odom";
-        // position
-        odom.pose.pose.position.x = enu[0];
-        odom.pose.pose.position.y = enu[1];
-        odom.pose.pose.position.z = 0.0; // enu[2];
-        // orientation
-        odom.pose.pose.orientation = get_orientation(enu, prec_pose);
-
-        pub.publish(odom);
-
-        // ROS_INFO("-----------------------%d\n   input gps: lat=%f , long=%f, height=%f \n   enu: x=%f , y=%f , z=%f \n ",odom_seq_id, latitude, longitude, altitude, odom.pose.pose.position.x, odom.pose.pose.position.y, odom.pose.pose.position.z);
-
-        odom_seq_id += 1;
-        prec_pose = enu;
-    }
-
-    std::vector<double> from_gps_to_ECEF(double latitude, double longitude, double altitude)
-    {
-        std::vector<double> ecef;
-
-        double a = 6378137;
-        double b = 6356752;
-
-        double e_square = 1 - ((b * b) / (a * a));
-        // ROS_INFO("----------e_square=%f ", e_square );
-
-        double n = this->n_calculation(latitude, e_square);
-
-        ecef.push_back((n + altitude) * cos(latitude) * cos(longitude)); // x
-        ecef.push_back((n + altitude) * cos(latitude) * sin(longitude)); // y
-        ecef.push_back((n * (1 - e_square) + altitude) * sin(latitude)); // z
-
-        return ecef;
-    }
-
-    std::vector<double> from_ECEF_to_ENU(std::vector<double> &ecef)
-    {
-        std::vector<double> enu;
-        std::vector<double> pose_diff;
-
-        pose_diff.push_back(ecef[0] - ref_ecef[0]); // x
-        pose_diff.push_back(ecef[1] - ref_ecef[1]); // y
-        pose_diff.push_back(ecef[2] - ref_ecef[2]); // z
-
-        enu = this->vectorial_mult(this->ref_matrix, pose_diff);
-
-        return enu;
-    }
-
-    void set_reference_system(double latitude, double longitude, double altitude)
-    {
-        ref_latitude = latitude;   // theta in radiants
-        ref_longitude = longitude; // lambda in radiants
-        ref_altitude = altitude;   // h in meters
-
-        // matrix
-        ref_matrix[0][0] = -sin(ref_longitude);
-        ref_matrix[0][1] = cos(ref_longitude);
-        ref_matrix[0][2] = 0;
-
-        ref_matrix[1][0] = -sin(ref_latitude) * cos(ref_longitude);
-        ref_matrix[1][1] = -sin(ref_latitude) * cos(ref_longitude);
-        ref_matrix[1][2] = cos(ref_latitude);
-
-        ref_matrix[2][0] = cos(ref_latitude) * cos(ref_longitude);
-        ref_matrix[2][1] = cos(ref_latitude) * sin(ref_longitude);
-        ref_matrix[2][2] = sin(ref_latitude);
-
-        ref_ecef = this->from_gps_to_ECEF(ref_latitude, ref_longitude, ref_altitude);
-        ref_enu = this->from_ECEF_to_ENU(ref_ecef);
-    }
-
-    double n_calculation(double phi, double e_square)
-    {
-        double a = 6378137;
-        double n = a / sqrt(1 - e_square * sin(phi) * sin(phi));
-
-        return n;
-    }
-
-    std::vector<double> vectorial_mult(std::vector<std::vector<double>> &matrix, std::vector<double> &vect)
-    {
-        std::vector<double> res(matrix.size(), 0);
-
-        for (int i = 0; i < matrix.size(); i++)
-        {
-            for (int j = 0; j < matrix.size(); j++)
-            {
-                res[i] += matrix[i][j] * vect[j];
-            }
-        }
-        return res;
-    }
-
-    geometry_msgs::Quaternion get_orientation(std::vector<double> curr_pose_enu, std::vector<double> prec_pose_enu)
-    {
-        double dx = curr_pose_enu[0] - prec_pose_enu[0];
-        double dy = curr_pose_enu[1] - prec_pose_enu[1];
-        double dz = curr_pose_enu[2] - prec_pose_enu[2];
+        double dx = curr_pose.x - prec_pose.x;
+        double dy = curr_pose.y - prec_pose.y;
+        double dz = curr_pose.z - prec_pose.z;
 
         // Calculate yaw (heading) angle
         double yaw = atan2(dy, dx);
@@ -189,6 +216,81 @@ public:
 
         return orientation;
     }
+
+public:
+
+    ros::NodeHandle n;
+    ros::Subscriber sub;
+    ros::Publisher pub;
+
+    bool flag;
+    int odom_seq_id;
+
+    Ref_syst ref;
+    ENU_coord prec_pose;
+
+
+
+    gps_to_odom()
+    {
+        flag = false;
+        sub = n.subscribe("/fix", 1, &gps_to_odom::callback, this);
+        pub = n.advertise<nav_msgs::Odometry>("/gps_odom", 1);
+
+        ros::Rate loop_rate(5);
+
+        while (ros::ok())
+        {
+            ros::spinOnce();
+            loop_rate.sleep();
+        }
+    }
+
+    void callback(const sensor_msgs::NavSatFix::ConstPtr &msg)
+    {   
+        gps_coord gps(msg->latitude, msg->longitude, msg->altitude); //gps format of msgs
+        ECEF_coord ecef(gps); // ECEF format of msg
+
+        nav_msgs::Odometry odom;
+        tf2::Quaternion quat_orientation;
+
+        if (!flag)
+        {
+            set_ref(gps, ecef);
+            ROS_INFO("----------reference system\n lat=%f , long=%f, height=%f\n ecef: x=%f , y=%f , z=%f\n enu: x=%f , y=%f , z=%f\n", msg->latitude, msg->longitude, msg->altitude, ecef.x, ecef.y, ecef.z, prec_pose.x, prec_pose.y, prec_pose.z  );
+            
+        }
+
+        ENU_coord enu(ecef, ref); //ENU format of msg
+                             //it is calculated here because to compute the ENU is necessary the ref system
+
+        // creating the odometry
+        // header
+        odom.header.seq = odom_seq_id;
+        odom.header.stamp = ros::Time ::now();
+        odom.header.frame_id = "gps_odom";
+        // position
+        odom.pose.pose.position.x = enu.x;
+        odom.pose.pose.position.y = enu.y;
+        odom.pose.pose.position.z = 0.0; // enu[2];
+        // orientation
+        odom.pose.pose.orientation = get_orientation(enu, prec_pose);
+
+        pub.publish(odom);
+
+        // ROS_INFO("-----------------------%d\n   input gps: lat=%f , long=%f, height=%f \n   enu: x=%f , y=%f , z=%f \n ",odom_seq_id, latitude, longitude, altitude, odom.pose.pose.position.x, odom.pose.pose.position.y, odom.pose.pose.position.z);
+
+        odom_seq_id += 1;
+        prec_pose = enu;
+    }
+
+    void set_ref(const gps_coord& gps, const ECEF_coord& ecef)
+    {
+        ref = Ref_syst(gps, ecef);
+        prec_pose = ENU_coord(ecef, ref);
+        flag = true;
+    }
+
 };
 
 int main(int argc, char **argv)
